2c1fe133e2d2f197069f4f090c2f76323fa60627
gothicprakhar <gothicprakhar@gmail.com>
2015-06-15 04:28:19 -0700

    Create def.php

---
+++ Day1/Part1/def.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+    
+<form action="insert.php" method="post">
+<h2>Form</h2>
+<label>FirstName:</label>
+<input class="input" name="fname" type="text" value="">
+<label>LastName:</label>
+<input class="input" name="lname" type="text" value="">
+<label>Email:</label>
+<input class="input" name="email" type="text" value="">
+<label>Password:</label>
+<input class="input" name="psswd" type="text" value="">
+<input class="submit" name="submit" type="submit" value="Submit">
+</form>
+    
+<form action="search.php" method="post">
+<h2>search</h2>
+<label>Name:</label>
+<input class="input" name="name" type="text" value="">
+<input class="submit" name="submit" type="submit" value="Search">
+</form>
+    
+</body>
+</html>
